e80a5c4aad58b64b733fb317694fd1288b324812
unknown <damian.gorka94@gmail.com>
2020-02-07 15:13:06 +0100

    Fixing login problem

--- src/main/java/com/ZooManagerApp/security/SecurityConfiguration.java
+++ src/main/java/com/ZooManagerApp/security/SecurityConfiguration.java
@@ -35,7 +35,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.httpBasic().and()
+                .authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/divisions/**").hasAnyRole("USER","ADMIN")
                 .and().formLogin();
